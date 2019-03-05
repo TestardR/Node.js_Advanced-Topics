71eb776eb9cac1d97ce2b666640ffd347a0b1a49
unknown <rmtestard@gmail.com>
2019-03-05 15:19:32 +0100

    not really single threaded

---
+++ main/loop.js
@@ -0,0 +1,33 @@
+// Fake code to show node's event loop
+
+// node myFile.js
+
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFile running
+myFile.runContents();
+
+function shouldContinue() {
+  // Check one: Any pending setTimeout, setInterval, setImmediate ?
+  // Check two: Any pending OS tasks ? (Like server listening to port)
+  // Check three: Any pending long running operations ? (Like fs module)
+  return (
+    pendingTimers.length || pendingOSTasks.length || pendingOperations.length
+  );
+}
+
+// Entire body executes in one 'tick'
+while (shouldContinue()) {
+  // 1) Node looks at pendingTimes and sees if any functions are ready to be called (setTimeout, setInterval)
+  // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+  // 3) Pause execution. Continue when...
+  // a. New pendingOSTask is done
+  // b. New pendingOperation is done
+  // c. A timer is about to complete
+  // 4) Look at pendingTimers. Call any setImmediate
+  // 5) Handle any 'close' events
+}
+
+// exit back to terminal
