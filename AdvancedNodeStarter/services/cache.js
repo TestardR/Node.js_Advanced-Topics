452c4e311ecfa101b64d14c4fc3a0ffb4a248850
unknown <rmtestard@gmail.com>
2019-03-15 16:16:30 +0100

    hydrating models

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,15 +1,36 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('I am about to run a query');
+mongoose.Query.prototype.exec = async function() {
+  // {} is the object we are copying properties to
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
 
-  console.log(key);
+  // If we do, return that
+  if (cacheValue) {
+    // convert to mongo format
+    const doc = new this.model(JSON.parse(cacheValue));
 
-  return exec.apply(this, arguments);
+    return doc;
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+
+  // Turn result into json and store it into redis
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
