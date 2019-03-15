6c1c67120bc46413b438c15248d09a67c39642af
unknown <rmtestard@gmail.com>
2019-03-15 16:20:22 +0100

    hydrating arrays

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -21,9 +21,9 @@ mongoose.Query.prototype.exec = async function() {
   // If we do, return that
   if (cacheValue) {
     // convert to mongo format
-    const doc = new this.model(JSON.parse(cacheValue));
+    const doc = JSON.parse(cacheValue);
 
-    return doc;
+    Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
   }
 
   // Otherwise, issue the query and store the result in redis
