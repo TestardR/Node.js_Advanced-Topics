8ab234b84722296baa6fdba66311b5949f7cc98f
unknown <rmtestard@gmail.com>
2019-03-19 16:37:22 +0100

    testing login

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -11,7 +11,7 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 test('the header has the correct text', async () => {
@@ -23,4 +23,26 @@ test('clicking login start oauth flow', async () => {
   await page.click('.right a');
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+  const id = '5c87a527b5f5323364fcbccc';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
 });
